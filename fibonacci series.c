7909e0c258e9bdf6726c555ebd56475cd421f56b
NIVED P V <92683901+nivedpv21040@users.noreply.github.com>
2022-10-10 23:26:17 +0530

    Add files via upload

---
+++ fibonacci series.c
@@ -0,0 +1,17 @@
+//program for fibonacci series
+#include <stdio.h>
+int main()
+{
+  int x = 1, y = 0, i;
+  printf("Series: ", x, y);
+  i = x+y;
+  while (i <= 150)
+  {
+    printf("%d, ", i);
+    x = y;
+    y = i;
+    i = x+y;
+  }
+
+  return 0;
+}
