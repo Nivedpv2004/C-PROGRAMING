506064e569f39d8dd4d3de4102aac9937813e5af
NIVED P V <92683901+nivedpv21040@users.noreply.github.com>
2022-10-10 23:07:09 +0530

    Add files via upload

---
+++ product of two numbers.c
@@ -0,0 +1,11 @@
+// program to find the product of two numbers
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("Product of two numbers is: %d", a * b);
+}
