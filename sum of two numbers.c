5f25e6b106c9455d97436290e277ac6bfe8cdf4f
NIVED P V <92683901+nivedpv21040@users.noreply.github.com>
2022-10-10 23:00:04 +0530

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,11 @@
+// program to find the sum of two numbers
+#include <stdio.h>
+int main()
+{
+    int a, b, sum;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("sum of two numbers is: %d", a + b);
+}
