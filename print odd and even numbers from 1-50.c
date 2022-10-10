437776f6faf94cdc6454cefee8a80a24acc2ae19
NIVED P V <92683901+nivedpv21040@users.noreply.github.com>
2022-10-10 23:22:29 +0530

    Add files via upload

---
+++ print odd and even numbers from 1-50.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+ int i;
+ int x=50;
+ printf("Even Numbers :");
+
+ i=2;
+ while(i <= x)
+ {
+  printf(" %d",i);
+  i = i + 2;
+ }
+ printf("\nOdd Numbers : ");
+ i=1;
+ while(i <= x)
+ {
+  printf(" %d",i);
+  i = i + 2;
+ }
+}
+
+
+
+
