a3d08c1246f38a44294082dc3386c1c383416938
Raja-s-k <92567732+Raja-s-k@users.noreply.github.com>
2022-10-16 16:59:33 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("Enter the length of the pool : ");
+    scanf("%d",&a);
+    printf("Enter the width of the pool : ");
+    scanf("%d",&b);
+    printf("Enter the depth of the pool : ");
+    scanf("%d",&c);
+    printf("The volume of pool is %d",a*b*c);
+    }
