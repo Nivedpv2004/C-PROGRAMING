a1cad911ede80e392444bd126b0b01f24ea42392
avanthika <avanthikamahe64@gmail.com>
2022-10-17 12:57:32 +0530

    add arrayrev.c

---
+++ arrayrev.c
@@ -0,0 +1,21 @@
+#include<stdio.h>  
+  
+#define N 5  
+  
+int main()  
+{  
+    int a[N], i, *ptr;  
+  
+    printf("Enter %d integer numbers\n", N);  
+    for(i = 0; i < N; i++)  
+        scanf("%d", &a[i]);  
+  
+    ptr = &a[N - 1];  
+  
+    printf("\nElements of array in reverse order ...\n");  
+    for(i = 0; i < N; i++)  
+        printf("%d\n", *ptr--);  
+  
+    return 0;  
+}  
+
