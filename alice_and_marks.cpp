1d886234d3f3699f4458df509f6ffe698ffabeea
Pranav Krishnan <pranavk0217@gmail.com>
2022-10-25 21:40:37 +0530

    Add files via upload

---
+++ alice_and_marks.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n, m; 
+	cin >> n >> m;
+	if(n >= 2*m){
+	    cout << "YES";
+	}
+	else{
+	    cout << "NO";
+	}
+	return 0;
+}
+
